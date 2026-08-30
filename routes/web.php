938c59edd4e79146c38746581441b0cf738d0851
Shota Miyazaki <myzk.study@gmail.com>
2026-08-30 14:31:43 +0900

    alpine.jsを使ってみる

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivewireTestController;
+use App\Http\Controllers\AlpineTestController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,3 +26,5 @@ Route::controller(LivewireTestController::class)
     Route::get('index', 'index')->name('index');
     Route::get('register', 'register')->name('register');
 });
+
+Route::get('alpine-test/index', [AlpineTestController::class, 'index']);
