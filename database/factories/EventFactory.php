9432667809d3c34e3fe257b369298c81161cc3e5
Shota Miyazaki <myzk.study@gmail.com>
2026-08-31 15:16:46 +0900

    event factory ダミーデータやらControllerなどを設定

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Event;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $dummyDate = $this->faker->dateTimeThisMonth;
+
+        return [
+            'name' => $this->faker->name,
+            'information' => $this->faker->realText,
+            'max_people' => $this->faker->numberBetween(1,20),
+            'start_date' => $dummyDate->format('Y-m-d H:i:s'),
+            'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'),
+            'is_visible' => $this->faker->boolean
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Event;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +21,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
         ]);
+
+        Event::factory(100)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
