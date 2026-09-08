10890c70513e5601021da2ece7da3a5c2a430346
Shota Miyazaki <myzk.study@gmail.com>
2026-09-09 02:16:14 +0900

    イベントのダミーデータの生成 トップページに一週間分のイベント情報を取得する処理を追加 カレンダー変更でイベント情報を取得する機能を追加

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AlpineTestController;
 use App\Http\Controllers\EventController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('calendar');
 });
 
 Route::middleware([
